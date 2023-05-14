992af5e81bbdaf6bef52db9e32d9c4ab7e8474f0
bismarkboateng <bismarkb609@gmail.com>
2023-05-14 22:25:45 +0000

    auth implementing...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import { Switch, Route, Link } from "react-router-dom";
 import { AddTodo, Signup, Login  } from "./components";
 import NavBar from "./components/NavBar";
+import React from "react";
 
 
 const App = () => {
+  const [user, setUser] = React.useState(null);
+  const [token, setToken] = React.useState(null);
+  const [error, setError] = React.useState("");
+
+
+  const login = async (user = null) => {
+    setUser(user);
+  }
+
+  const logout = async () => {
+    setUser(null);
+  }
+
+  const singup = async (user = null) => {
+    setUser(user);
+  }
+
+
   return (
     <div>
       <NavBar />
