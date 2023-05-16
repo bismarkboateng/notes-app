a9ac7d6868de32b085f8650a565ee7d6f2c5c214
bismarkboateng <bismarkb609@gmail.com>
2023-05-16 11:35:59 +0000

    listing todo by user

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,9 +1,56 @@
-const TodoList = () => {
+import React from "react";
+import TodoDataService from "../services/todos";
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+const Todos = new TodoDataService();
+
+
+const TodoList = (props) => {
+  const [todos, setTodo] = React.useState([]);
+
+  React.useEffect(() => {
+    retrieveTodos();
+  },[props.token]);
+
+  const retrieveTodos = () => {
+    Todos.getAll(props.token)
+      .then(response => {
+        setTodo(response.data);
+      })
+      .catch( e => {
+        console.log(e);
+      });
+  }
+
   return (
-    <div>
-      Todo List
+    <div className="m-10">
+      { props.token === null || props.token === ""
+      ? (<div className="bg-red-100 p-10 rounded text-red-500"> You are not Logged in: Please <Link to="/login">login </Link>to see your todos</div>)
+      : ( 
+        <div>
+          { todos.map((todo) => (
+            <div className="p-2">
+              <div key={todo.id}>
+                <h1>{ todo.title }</h1>
+                <strong>Memo: </strong> <span>{ todo.memo }</span>
+              
+                <p>Date created: { moment(todo.created_at).format("Do MMMM YYYY")}  </p>
+                <div className="mt-2">
+                  <Link to={`/todos/${todo.id}`} className="mr-3">
+                    <button className="bg-blue-500 text-white px-5 py-1 rounded">Edit</button>
+                  </Link>
+                  <button className="bg-red-500 text-white px-5 py-1 rounded" >Delete</button>
+                  <hr className="mt-3 mb-3" />
+                </div>
+              </div>  
+            </div>
+          ))}
+        </div>
+      )
+    }
     </div>
   )
 }
 
-export default TodoList
+export default TodoList;
